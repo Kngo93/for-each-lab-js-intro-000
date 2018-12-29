b6037c534b731d14baf77da5976bdcdceabaf924
Kevin Ngo <k.ngo93@outlook.com>
2018-12-29 23:19:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function iterativeLog(array){
     console.log(index + ": " + element);
   });
 }
+
+function log(var){
+  console.log(var * var);
+}
+
+function iterate(log) {
+  var num = [1, 2, 3];
+
+  num.forEach(log());
+}
